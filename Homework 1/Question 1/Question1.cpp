2c96338ca510558c71b9b03a79caa8fd0e533697
jaydenslotnick <jcs6472@psu.edu>
2024-01-28 21:17:12 -0500

    Attempt to finish Q1, has an infinite loop problem

--- Homework 1/Question 1/Question1.cpp
+++ Homework 1/Question 1/Question1.cpp
@@ -59,25 +59,73 @@ int main()
 	std::cout << "Enter baggage weight (pounds): " << std::endl;
 	std::cin >> baggage_weight;
 	std::cout << "Enter baggage moment arm: " << std::endl;
-	std::cin >> baggage_weight;
+	std::cin >> baggage_moment_arm;
 
 
+	// Allocates variables for the gross weight and cg location
+	double gross_weight = empty_weight;
+	double total_moment = empty_weight * empty_weight_moment;
 
+	// Sets limits for gross weight and cg location
+	double max_gross_weight = 2950; // pounds
+	double forward_cg_limit = 82.1; // inches
+	double aft_cg_limit = 84.7; // inches
+
+	for (int i = 0; i < front_occupants; ++i)
+	{
+		gross_weight += weight_front_occupants[i];
+		total_moment += weight_front_occupants[i] * front_seat_moment_arm;
+	}
+
+
+	for (int i = 0; i < rear_occupants; ++i)
+	{
+		gross_weight += weight_rear_occupants[i];
+		total_moment += weight_rear_occupants[i] * rear_seat_moment_arm;
+	}
+
+
+	gross_weight += usable_fuel * weight_usable_fuel + baggage_weight;
+	total_moment += usable_fuel * weight_usable_fuel * fuel_tank_moment_arm + baggage_weight * baggage_moment_arm;
+
+	double cg_location;
+	cg_location = total_moment / gross_weight;
+
+	if (gross_weight < max_gross_weight && cg_location >= forward_cg_limit && cg_location <= aft_cg_limit)
+	{
+		std::cout << "The plane is within the given design limits" << std::endl;
+
+	}
+	else 
+	{
+		std::cout << "The plane is not within the given design limits" << std::endl;
+	}
 
 	
+	double fuel_change = 0;
+	do 
+	{
+
+		if (gross_weight > max_gross_weight)
+		{
+			fuel_change = gross_weight - max_gross_weight;
+			gross_weight = max_gross_weight;
+		}
+
+		if (cg_location < forward_cg_limit)
+		{
+			fuel_change += 0.01;
+		}
 
 
+		else if (cg_location > aft_cg_limit)
+		{
+			fuel_change -= 0.01;
+		}
 
+	} while (gross_weight > max_gross_weight || cg_location < forward_cg_limit || cg_location > aft_cg_limit);
 
-
-
-
-
-
-
-
-
-
+	std::cout << "Adjusted fuel to meet design limits: " << fuel_change << std::endl;
 
 
 
