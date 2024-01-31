2e51acbe3da2fc21755e74c0472f38a87fdb035a
jaydenslotnick <jcs6472@psu.edu>
2024-01-31 10:57:00 -0500

    Question 2

--- Homework 1/Question 2/Question2.cpp
+++ Homework 1/Question 2/Question2.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
-#include <array>
+#include <map>
 
 int main()
 {
+	// Creates a map container with the name of the flight as the key
+	// and the flight distance as the value
 
-
-
+	// The three flights and values were set as new elements
+	std::map< std::string, double > flights;
+	flights.insert(std::make_pair("SCE_PHL", 160)); // State college and Philly
+	flights.insert(std::make_pair("SCE_ORD", 640)); // State college and Chicago
+	flights.insert(std::make_pair("SCE_EWR", 220)); // State college and Newark
 
 	return 0;
 }
